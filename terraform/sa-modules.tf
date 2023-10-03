e2d5f75e87468d8b3ef958f9cc74f3490adfdf98
Riley Snyder <riley.snyder@harness.io>
2023-10-03 11:22:09 -0500

    remove ci factory, fix infra namespace

--- terraform/sa-modules.tf
+++ terraform/sa-modules.tf
@@ -1,21 +1,21 @@
-module "harness-ci-factory" {
-  source = "git@github.com:harness-community/solutions-architecture.git//harness-ci-factory?ref=main"
-  # source = "../../solutions-architecture/harness-ci-factory"
+# module "harness-ci-factory" {
+#   source = "git@github.com:harness-community/solutions-architecture.git//harness-ci-factory?ref=main"
+#   # source = "../../solutions-architecture/harness-ci-factory"
 
-  harness_api_key_secret           = "account.harness_api_token"
-  organization_name                = module.solution-architecture.details.name
-  create_organization              = false
-  project_name                     = "harness-ci-factory"
-  create_project                   = true
-  container_registry               = "registry.rileysnyder.dev"
-  container_registry_type          = "docker"
-  container_registry_connector_ref = "account.hurley"
-  kubernetes_connector_ref         = "account.zirawork"
-  kubernetes_namespace             = "harness-delegate-ng-work"
-  max_build_concurrency            = 5
-  enable_schedule                  = false
-  schedule                         = "0 2 * * *"
-}
+#   harness_api_key_secret           = "account.harness_api_token"
+#   organization_name                = module.solution-architecture.details.name
+#   create_organization              = false
+#   project_name                     = "harness-ci-factory"
+#   create_project                   = true
+#   container_registry               = "registry.rileysnyder.dev"
+#   container_registry_type          = "docker"
+#   container_registry_connector_ref = "account.hurley"
+#   kubernetes_connector_ref         = "account.zirawork"
+#   kubernetes_namespace             = "harness-delegate-ng-work"
+#   max_build_concurrency            = 5
+#   enable_schedule                  = false
+#   schedule                         = "0 2 * * *"
+# }
 
 # module "terraform-development-factory" {
 #   # source = "git@github.com:harness-community/solutions-architecture.git//terraform-development-factory?ref=enhancement/update-module-source"
