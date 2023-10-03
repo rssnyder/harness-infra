ad1dfd19a4041a6d31623f0b9ca773a4871171a0
Riley Snyder <riley.snyder@harness.io>
2023-10-03 11:26:58 -0500

    add random provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
